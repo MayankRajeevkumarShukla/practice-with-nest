969b31bb32664c1b4d04540163d8bba10a41f192
MayankRajeevkumarShukla <97381684+MayankRajeevkumarShukla@users.noreply.github.com>
2025-09-13 11:12:23 +0530

    add Swagger configuration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Nest API Playground')
+    .setDescription('Test your routes here')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // UI at /api
+
+  await app.listen(3000);
 }
-bootstrap().catch((err) => {
-  console.error('Error starting Nest app:', err);
-  process.exit(1);
-});
+bootstrap();
